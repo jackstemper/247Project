cfc254eafbba828202d32d51adb050cf51d53cf2
jhill08 <jah18@email.sc.edu>
2020-04-06 20:04:45 -0400

    Update User.java

--- MovieApp/src/User.java
+++ MovieApp/src/User.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
 
 public class User extends Account{
+	protected User user;
+	protected String Username;
+	protected String password;
 	protected Ticket[] Tickets;
 	public Ticket[] getTickets() {
 		return Tickets;
 	}
 	public void setTickets(Ticket[] tickets) {
 		Tickets = tickets;
+	}
+
+	public User(){
+		user = new User;
+		user.Username = "User";
+		user.Password = "Default";
 	}
 	
 }

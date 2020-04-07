10cdf03275430a04f0e0d36306495fb0c340fbb7
jhill08 <jah18@email.sc.edu>
2020-04-06 20:04:42 -0400

    Update Manager.java

--- MovieApp/src/Manager.java
+++ MovieApp/src/Manager.java
@@ -1,7 +1,18 @@
 
 public class Manager extends Account{
+	protected Manager manager;
+	protected String Username;
+	protected String Password;
+	protected boolean isManager;
 	protected Movie[] Movies;
 	public void changeEvent(Movie) {
 		return;
+	}
+
+	public Manager(){
+		manager = new Manager;
+		manager.Username = "Manager";
+		manager.Password = "Password";
+		isManager = true;
 	}
 }

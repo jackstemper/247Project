722eeb8d6012b5b36b01ade7ed1e507302b1045e
jackstemper <30327455+jackstemper@users.noreply.github.com>
2020-04-08 17:48:58 -0400

    This is a test on the thing that I think is almost right, but idk whats going on with it

--- MovieApp/src/MovieDriver.java
+++ MovieApp/src/MovieDriver.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 /*
 * MovieDriver class : Holds an array of Home and Venue Options that give the basic functions of the app, runs the 
@@ -5,7 +6,6 @@ import java.util.Scanner;
 */
 public class MovieDriver {
 	private Scanner scanner;
-	private MovieApp movieApp;
 	// View shows the Name and Addresses of the theaters in the theaters[]
 	// Checkout goes to a theater for Venue Options
 	// Make Account makes it so that you can get a ticket[] to save the tickets you get in a session
@@ -15,14 +15,47 @@ public class MovieDriver {
 	// Buy Ticket should increase the capacity counter in the Movie/play, and return a ticket to the account Ticket[]
 	// GbttHS is obv, just break out to Home Screen
 	private String[] VenueOptions = {"View Movies", "Buy a Ticket", "Go back to the Home Screen"};
-	
+	protected Theater[] Theaters;
+	protected Auditorium[] Auditoriums;
 	MovieDriver(){
 		scanner = new Scanner(System.in);
-		movieApp = new MovieApp();
+	}
+	
+	//loading test movie and theater information
+	public void load() {
+		Movie Frozen = new Movie("Frozen 2", 9.50, 6.50, "AMC", 60, 0, "Elsa");
+		Movie Jungle = new Movie("The Jungle Book", 9.50, 5.0, "AMC", 60, 0, "Mowgli");
+		Jungle.print();
+		Movie[] AMCList = {Frozen, Jungle};
+		Theater AMC = new Theater("AMC", "6969 Test Dr.", AMCList);
+		Theaters[0]=AMC;
+		Movie Matrix = new Movie("The Matrix", 11.50, 8.50, "Carmike", 40, 0, "Neo");
+		Movie Community = new Movie("Community: the Movie", 11.50, 7.0,"Carmkie", 40, 0, "Abhed");
+		Movie[] CarmikeList = {Matrix, Community};
+		Theater Carmike = new Theater("Carmike", "457984 Greendale Rd.", CarmikeList);
+		Theaters[1]=Carmike;
+	}
+	public void viewTheaters() {
+		System.out.println(Arrays.toString(Theaters));
+	}
+	public void goToTheater() {
+		Venue venue = new Venue();
+		venue.getAddress();
+	}
+	public void printAMovie(Movie movie) {
+		
+	}
+	public void makeAccount() {
+		User user = new User();
+		user.createAccount(user);
+	}
+	public void displayTix() {
+		Ticket ticket = new Ticket();
+		ticket.printTicket();
 	}
 	public void run() {
 		System.out.println("Loading the App");
-		movieApp.load();
+		load();
 		System.out.println("Welcome to our Columbia Movie Theater App");
 		while(true) {
 			displayHome();
@@ -30,16 +63,16 @@ public class MovieDriver {
 			if(userCommand == HomeOptions.length -1) break;
 			switch(userCommand) {
 				case(0):
-					movieApp.viewTheaters();
+					viewTheaters();
 					break;
 				case(1):
-					movieApp.printAMovie(movieApp.Theaters[1].Movies[1]);
+					printAMovie(Theaters[1].Movies[1]);
 					break;
 				case(2):
-					movieApp.makeAccount();
+					makeAccount();
 					break;
 				case(3):
-					movieApp.displayTix();
+					displayTix();
 					break;
 			}
 		}

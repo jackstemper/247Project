80fae339a40021e9949f866470637d6b7cf129e8
jtscholz <jtscholz@email.sc.edu>
2020-04-08 01:36:06 -0400

    Update PArts.java

--- MovieApp/src/PArts.java
+++ MovieApp/src/PArts.java
@@ -1,4 +1,7 @@
-
+/*
+* PArts Class : extends Shows, functions for setting and getting the Group performing,
+* prints details of the shows title and the group performing 
+*/
 public class PArts extends Shows {
 	protected String Group;
 	public String getGroup() {
